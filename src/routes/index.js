fdca79dcea9e64c07d70fbdb4fd39a7b955590b8
Keval Khatri <kevalkhatri32@gmail.com>
2024-04-05 06:30:25 -0700

    Setting temp code file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,11 @@
-// import { lazy } from 'react';
-// import CommonLayout from 'layout/CommonLayout';
-// import Loadable from 'components/Loadable';
-// const PagesLanding = Loadable(lazy(() => import('pages/landing')));
 import { useRoutes } from 'react-router-dom';
 
 // project-imports
 import LoginRoutes from './LoginRoutes';
 import MainRoutes from './MainRoutes';
 
-// render - landing page
-
 // ==============================|| ROUTES RENDER ||============================== //
 
 export default function ThemeRoutes() {
-  return useRoutes([
-    // {
-    //   path: '/',
-    //   element: <CommonLayout />,
-    //   children: [
-    //     {
-    //       path: '/',
-    //       element: <PagesLanding />
-    //     }
-    //   ]
-    // },
-    LoginRoutes,
-    MainRoutes
-  ]);
+  return useRoutes([LoginRoutes, MainRoutes]);
 }
